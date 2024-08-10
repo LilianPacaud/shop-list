6572035ee5c888c6c947c090db9181e060749b36
Lilian PACAUD <lilian.pacaud@alten.com>
2024-08-11 00:28:45 +0200

    Set version 1.0

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "shop-list-app",
     slug: "shop-list-app",
-    version: "0.5.1",
+    version: "1.0",
     sdkVersion: "51.0.0",
     platforms: ["ios", "android"],
     orientation: "portrait",
